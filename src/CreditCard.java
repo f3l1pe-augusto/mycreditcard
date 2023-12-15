dbbf1b46739f7bf6b2993ce5bcb1da8ee8d0e562
f3l1pe-augusto <fa.ferreira@unesp.br>
2023-12-15 09:53:44 -0300

    Creation of class CreditCard

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,5 +1,37 @@
 public class CreditCard {
-    public static void main(String[] args) {
-        
+    private final int number;
+    private String name;
+    private final String bank;
+    private double limit;
+
+    public CreditCard(final int number, String bank, double limit) {
+        this.number = number;
+        this.bank = bank;
+        this.limit = limit;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    @Override
+    public String toString() {
+        return "Credit card number: " + this.getNumber() + "\nBank: " + this.getBank() + "\nLimit: " + this.getLimit();
     }
 }
